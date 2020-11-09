e2bf700ce290d7177671df18382e7d263d2dafd9
gmzi <gastonmazieres@gmail.com>
2020-11-09 11:05:51 -0500

    comments added

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Change background color when moving the cursor (Unit 4.3.8).
 /* I'm trying to build some logic to have all colors with just moving the cursor, 
 I've managed to make full red (255, 0, 0) and full blue (0, 0, 255) but couldn't 
-make full magenta (0, 255, 0), I guess the problem is where `help` commments
-are, but honestly I'm lost */
+make full magenta (0, 255, 0), */
 
 const body = document.querySelector('body');
 body.addEventListener('mousemove', function (e) {
+  // make the color:
   let width = Math.round((e.pageX * 255) / window.innerWidth);
   let height = Math.round((e.pageY * 255) / window.innerHeight);
-  // getting lost here:
+  // (getting so lost here):
   let diagonal = Math.round(width + height / 255);
   let r = 255 - diagonal;
   let g = width - height;
@@ -19,6 +19,7 @@ body.addEventListener('mousemove', function (e) {
     b = width;
   }
 
+  // values assignment:
   let color = `rgb(${r}, ${g}, ${b})`;
   body.style.backgroundColor = color;
 });
